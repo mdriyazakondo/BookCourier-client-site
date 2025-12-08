3555ae4d72365dc369507576d1827c9c19043f2b
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-12-08 19:26:33 +0600

    leaflet map

---
+++ src/components/Home/Coverage/Coverage.jsx
@@ -0,0 +1,56 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import Container from "../../../shared/Container/Container";
+import "leaflet/dist/leaflet.css";
+
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+// Rangpur Division Districts
+const rangpurDistricts = [
+  { name: "Rangpur", position: [25.7439, 89.2752] },
+  { name: "Dinajpur", position: [25.6217, 88.635] },
+  { name: "Kurigram", position: [25.8054, 89.636] },
+  { name: "Nilphamari", position: [25.931, 88.856] },
+  { name: "Lalmonirhat", position: [25.9923, 89.2847] },
+  { name: "Thakurgaon", position: [26.033, 88.4617] },
+  { name: "Panchagarh", position: [26.3354, 88.5516] },
+  { name: "Gaibandha", position: [25.3287, 89.528] },
+];
+
+const Coverage = () => {
+  // Center to Rangpur Division area
+  const center = [25.7439, 89.2752];
+
+  return (
+    <Container>
+      <div style={{ height: "500px" }}>
+        <MapContainer
+          center={center}
+          zoom={8}
+          scrollWheelZoom={true}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {rangpurDistricts.map((d, idx) => (
+            <Marker key={idx} position={d.position}>
+              <Popup>{d.name}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
+    </Container>
+  );
+};
+
+export default Coverage;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Latest from "../../components/Home/Latest/Latest";
 import HeroSection from "../../components/Home/HeroSection/HeroSection";
 import WhyChooseBookCourier from "../../components/Home/WhyChooseBookCourier/WhyChooseBookCourier";
+import Coverage from "../../components/Home/Coverage/Coverage";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <Latest />
       <WhyChooseBookCourier />
+      <Coverage />
     </div>
   );
 };
