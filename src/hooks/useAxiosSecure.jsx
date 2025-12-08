c2b6f54121ded1f1748369cbfca7c5e654b22283
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-12-08 16:45:02 +0600

    base url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://server-pi-mocha.vercel.app",
+  baseURL: "http://server-pi-mocha.vercel.app",
   withCredentials: true,
 });
 
